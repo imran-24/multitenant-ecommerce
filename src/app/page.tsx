3af313c36be1606172bd30dea4deaffe615ad0aa
Imran Syam <md.imransyam1956@gmail.com>
2025-05-19 12:22:55 +0600

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className='flex flex-col space-y-6 p-4'>
+      <div>
+        <Button variant={"elevated"} >Click me</Button>
+      </div>
+      <div>
+        <Input placeholder='Search...' />
+      </div>
+      <div>
+        <Textarea placeholder='Search...' />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
